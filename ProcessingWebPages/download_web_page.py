67b24b60010eef32fdb8d08b087a3c0061ef732c
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-02-11 23:49:12 +0400

    Подсчёт слов, встречающихся тлько в видимом тексте страницы

--- ProcessingWebPages/download_web_page.py
+++ ProcessingWebPages/download_web_page.py
@@ -1,5 +1,17 @@
 from urllib.request import urlopen
 
-html = urlopen('https://stepik.org/media/attachments/lesson/209717/1.html').\
+html = urlopen('https://ru.wikipedia.org/wiki/Python'). \
     read().decode('utf-8')
 str_html = str(html)
+ans = []
+state = 0
+for c in str_html:
+    if c == '<':
+        state = 1
+    elif c == '>':
+        state = 0
+    if state == 0:
+        ans.append(c)
+text_page = ''.join(ans)
+
+print(text_page.count('C++'))
