75d1162986ee4bab22a3ef71c767e21ca9653d5e
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-02-13 00:08:44 +0400

    Реализовал вывод встречающихся строк между тегами code

--- ProcessingWebPages/download_web_page.py
+++ ProcessingWebPages/download_web_page.py
@@ -1,17 +1,32 @@
 from urllib.request import urlopen
+from collections import Counter
 
-html = urlopen('https://ru.wikipedia.org/wiki/Python'). \
+html = urlopen('https://stepik.org/media/attachments/lesson/209719/2.html'). \
     read().decode('utf-8')
 str_html = str(html)
 ans = []
-state = 0
-for c in str_html:
-    if c == '<':
-        state = 1
-    elif c == '>':
-        state = 0
-    if state == 0:
-        ans.append(c)
-text_page = ''.join(ans)
+start_tag_code = 0
+end_tag_code = 0
+i = 0
+while i < len(str_html):
+    if str_html.find('<code>', end_tag_code) > -1:
+        start_tag_code = str_html.find('<code>', end_tag_code)
+        end_tag_code = str_html.find('</code>', start_tag_code)
+        text_code = str_html[start_tag_code + 6: end_tag_code]
+        ans.append(text_code)
+        i = end_tag_code
+    else:
+        i = len(str_html)
 
-print(text_page.count('C++'))
+count = Counter(ans)
+max_v = 0
+for k, v in count.items():
+    if v > max_v:
+        max_v = v
+
+result = []
+for k, v in count.items():
+    if v == max_v:
+        result.append(k)
+
+print(' '.join(result))
